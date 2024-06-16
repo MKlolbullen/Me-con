b5816d70d589739657cc174c7883b48eb6949b56
MKlolbullen <53312929+MKlolbullen@users.noreply.github.com>
2024-06-16 08:18:23 +0200

    Update dork_list.py

---
+++ dorks_list.py
@@ -0,0 +1,158 @@
+#/bin/python3
+unique_dork_searches_continued = [
+
+    "intitle:'HFS' 'http file server'",
+
+    "inurl:'/CFIDE/administrator/'",
+
+    "site:*.edu inurl:login",
+
+    "inurl:'/plesk-stat/webstat'",
+
+    "inurl:'weblogic/index.jsp'",
+
+    "inurl:'/secure/ContactAdministrators!default.jspa'",
+
+    "site:*.net intext:'index of /' '.htaccess'",
+
+    "inurl:'/wp-content/plugins' filetype:php 'just another wordpress site'",
+
+    "inurl:'/irj/portal'",
+
+    "inurl:'/sap/bc/webdynpro'",
+
+    "inurl:'/SAML2/SSO/POST'",
+
+    "inurl:'/phpPgAdmin/browser.php'",
+
+    "inurl:'/NSearch/AdminServlet'",
+
+    "inurl:'/owa/auth/logon.aspx'",
+
+    "intitle:'index of' '*.pem'",
+
+    "site:*.com filetype:xml 'sitemap'",
+
+    "intitle:'index of' '*.key'",
+
+    "inurl:'manage.py' site:github.com",
+
+    "inurl:'jmx-console'",
+
+    "filetype:sql site:gov 'email'",
+
+    "inurl:'/zabbix.php' intitle:'Zabbix'",
+
+    "intitle:'index of' 'nginx.conf'",
+
+    "inurl:'/phpLiteAdmin.php'",
+
+    "intitle:'apache2 ubuntu default page'",
+
+    "site:*.com inurl:wp-json wp v2 users",
+
+    "intext:'index of /' '.bash_history'",
+
+    "inurl:admin intitle:index.of",
+
+    "inurl:/proc/self/cwd",
+
+    "intitle:'Proudly Served by Suricata'",
+
+    "inurl:'editor/list.asp' 'inurl:admin'",
+
+    "intitle:'Index Of' -inurl:maillog maillog size",
+
+    "site:gov 'public records' filetype:xls",
+
+    "inurl:'resetpassword.asp'",
+
+    "inurl:'user/login' site:edu",
+
+    "site:edu 'academic calendar' filetype:pdf",
+
+    "intitle:'webmin' inurl:10000",
+
+    "inurl:admin filetype:db",
+
+    "intitle:'document repository' site:gov filetype:pdf",
+
+    "intitle:'usage statistics' 'index of'",
+
+    "inurl:'phpinfo.php' filetype:php",
+
+    "filetype:log access.log -sample",
+
+    "intitle:'index of' 'mysql.conf' OR 'mysql_config'",
+
+    "site:org 'annual report' filetype:pdf",
+
+    "inurl:'temp' filetype:log",
+
+    "site:edu 'research papers' filetype:doc",
+
+    "inurl:'port_255' filetype:log",
+
+    "intitle:'network camera' inurl:'guest'",
+
+    "intitle:'index of' 'admin'",
+
+    "filetype:sql 'insert into' (pass|passwd|password)",
+
+    "intitle:'index of' 'parent directory'",
+
+    "site:edu exam filetype:pdf",
+
+    "inurl:'.env' filetype:env",
+
+    "site:gov confidential filetype:xls",
+
+    "intext:'vulnerable' filetype:php",
+
+    "inurl:'web.config' filetype:config",
+
+    "filetype:cfg autoconfig log",
+
+    "intitle:'live view' index of",
+
+    "intext:'confidential' filetype:doc",
+
+    "inurl:'wp-content/uploads' filetype:pdf",
+
+    "intitle:'index of' passwd",
+
+    "inurl:'cgi-bin' filetype:txt",
+
+    "site:ac.uk 'lecture notes' filetype:pdf",
+
+    "filetype:log username putty",
+
+    "intitle:'index of' 'backup'",
+
+    "filetype:xls inurl:'email.xls'",
+
+    "inurl:'/.git' site:github.com",
+
+    "intext:'database dump' filetype:sql",
+
+    "inurl:'user/login' site:edu",
+
+    "site:io software filetype:pdf",
+
+    "intitle:'EvoCam' inurl:'webcam.html'",
+
+    "inurl:/phpinfo.php",
+
+    "filetype:env DB_PASSWORD",
+
+    "intitle:'webcam 7' inurl:8080",
+
+    "site:edu inurl:thesis filetype:pdf",
+
+    "inurl:admin intitle:login",
+
+    "site:gov inurl:reports filetype:pdf",
+
+    "intitle:index.of confidential
+
+]
